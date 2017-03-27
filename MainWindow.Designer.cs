13e3c45cf4677ce903839cc8a0f80a02339729ad
DEV <devesh.shyngle@gmail.com>
2017-03-27 23:31:19 +0530

    v0.0.1
    First Commit

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace TheLocksmith
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DataBox = new System.Windows.Forms.TextBox();
+            this.KeyBox = new System.Windows.Forms.TextBox();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AnsBox = new System.Windows.Forms.TextBox();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The Locksmith";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DataBox
+            // 
+            this.DataBox.Location = new System.Drawing.Point(74, 29);
+            this.DataBox.Multiline = true;
+            this.DataBox.Name = "DataBox";
+            this.DataBox.Size = new System.Drawing.Size(213, 86);
+            this.DataBox.TabIndex = 1;
+            // 
+            // KeyBox
+            // 
+            this.KeyBox.Location = new System.Drawing.Point(74, 145);
+            this.KeyBox.Name = "KeyBox";
+            this.KeyBox.Size = new System.Drawing.Size(213, 26);
+            this.KeyBox.TabIndex = 2;
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EncryptButton.Location = new System.Drawing.Point(93, 284);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(196, 44);
+            this.EncryptButton.TabIndex = 3;
+            this.EncryptButton.Text = "Encrypt!";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.DataBox);
+            this.groupBox1.Controls.Add(this.KeyBox);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(29, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 199);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rjindael256 Encryption";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // AnsBox
+            // 
+            this.AnsBox.Location = new System.Drawing.Point(29, 351);
+            this.AnsBox.Multiline = true;
+            this.AnsBox.Name = "AnsBox";
+            this.AnsBox.ReadOnly = true;
+            this.AnsBox.Size = new System.Drawing.Size(318, 51);
+            this.AnsBox.TabIndex = 6;
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopyButton.Location = new System.Drawing.Point(130, 425);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(120, 31);
+            this.CopyButton.TabIndex = 7;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 482);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.AnsBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.label1);
+            this.Name = "MainWindow";
+            this.Text = "Encrypter/Decrypter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DataBox;
+        private System.Windows.Forms.TextBox KeyBox;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AnsBox;
+        private System.Windows.Forms.Button CopyButton;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+
+namespace TheLocksmith
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+        public static string Encrypt(string textToEncrypt, string EncryptionPassword)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+            rijndaelCipher.KeySize = 256;
+            rijndaelCipher.BlockSize = 128;
+            byte[] pwdBytes = Encoding.UTF8.GetBytes(EncryptionPassword);
+            pwdBytes = SHA256.Create().ComputeHash(pwdBytes);
+            byte[] keyBytes = new byte[16];
+            int len = pwdBytes.Length;
+            if (len > keyBytes.Length)
+            {
+                len = keyBytes.Length;
+            }
+            Array.Copy(pwdBytes, keyBytes, len);
+            rijndaelCipher.Key = keyBytes;
+            rijndaelCipher.IV = keyBytes;
+            ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
+            byte[] plainText = Encoding.UTF8.GetBytes(textToEncrypt);
+            return Convert.ToBase64String(transform.TransformFinalBlock(plainText, 0, plainText.Length));
+        }
+    }
+}
